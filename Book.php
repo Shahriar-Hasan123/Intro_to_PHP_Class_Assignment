315b41097435b0b842fbb991e33e676472124106
Shahriar Hasan <shahriar19735@gmail.com>
2023-10-17 14:27:01 +0600

    1st update

--- Book.php
+++ Book.php
@@ -18,19 +18,31 @@ class Book
     {
         return $this->isbn;
     }
+    public function setIsbn($isbn)
+    {
+        $this->isbn = $isbn;
+    }
     public function getTitle()
     {
         return $this->title;
+    }
+    public function setTitle($title)
+    {
+        $this->title = $title;
     }
     public function getAuthor()
     {
         return $this->author;
     }
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
     public function getAvailable()
     {
         return $this->available;
     }
-    private function setAvailable($available)
+    public function setAvailable($available)
     {
         $this->available = $available;
     }
--- Customer.php
+++ Customer.php
@@ -18,19 +18,31 @@ class Customer
     {
         return $this->id;
     }
+    public function setID($id)
+    {
+        $this->id = $id;
+    }
     public function getFirstName()
     {
         return $this->firstName;
+    }
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
     }
     public function getLastName()
     {
         return $this->lastName;
     }
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
     public function getEmail()
     {
         return $this->email;
     }
-    private function setEmail($email)
+    public function setEmail($email)
     {
         $this->email = $email;
     }
